30d8201542f900fcc1bef97bf85c6b9cb637293e
melissa <melissa160@gmail.com>
2016-11-28 18:25:35 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class StoryBox extends React.Component{
+  render(){
+    return(
+	    <div>
+	      <h3> McCircuit is my name</h3>
+        <p className="message">I am here to help. </p>
+	    </div>
+    );
+  }
+}
+
+export default StoryBox;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+
+var config = {
+   entry: './main.js',
+	
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            },
+         },
+         {
+            test: /\.css$/,
+            exclude: /\.useable\.css$/,
+            loader: "style-loader!css-loader"  
+         },
+         { 
+            test: /\.useable\.css$/, 
+            loader: "style-loader/useable!css-loader" 
+         }
+      
+      ]
+   }
+}
+
+module.exports = config;
