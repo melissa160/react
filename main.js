63c66d95cb29831483678f077a49e2124b02dbb3
melissa <melissa160@gmail.com>
2016-12-01 19:50:42 -0500

    adding .gitignore file

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentBox from './App.jsx';
+import './style.css'
 require("./style.css");
 ReactDOM.render(<CommentBox />, document.getElementById('app'));
